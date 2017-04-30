64371c71d180436af9c0ea745640769d6f3a1a7b
kwekuboateng <fredshavnqweku@gmail.com>
2017-04-30 19:37:18 +0000

    added access

--- index.js
+++ index.js
@@ -100,7 +100,7 @@ function sendTextMessage(recipientId, messageText) {
 function callSendAPI(messageData) {
   request({
     uri: 'https://graph.facebook.com/v2.6/me/messages',
-    qs: { access_token: FB_ACCESS_TOKEN },
+    qs: { access_token: access },
     method: 'POST',
     json: messageData
 

b2c4916fe59c46aedaaf997a5f63084e3c15082a
kwekuboateng <fredshavnqweku@gmail.com>
2017-04-30 19:09:21 +0000

    added send and receive message

--- index.js
+++ index.js
@@ -4,6 +4,9 @@ const request = require('request');
 const config = require('config');
 const aidahCare = express()
 
+const token = process.env.FB_VERIFY_TOKEN
+const access = process.env.FB_ACCESS_TOKEN
+
 aidahCare.set('port', (process.env.PORT || 5000))
 aidahCare.use(bodyParser.urlencoded({extended: false}))
 aidahCare.use(bodyParser.json())
@@ -13,11 +16,79 @@ aidahCare.get('/', function(req,res){
 })
 
 aidahCare.get('/webhook', function(req, res){
-	if(req.query['hub.verify_token'] === 'boateng'){
+	if(req.query['hub.verify_token'] === token){
 		res.send(req.query['hub.challenge'])
 	}
 	res.send('No entry')
 })
+
+aidahCare.post('/webhook', function (req, res) {
+  var data = req.body;
+
+  // Make sure this is a page subscription
+  if (data.object === 'page') {
+
+    // Iterate over each entry - there may be multiple if batched
+    data.entry.forEach(function(entry) {
+      var pageID = entry.id;
+      var timeOfEvent = entry.time;
+
+      // Iterate over each messaging event
+      entry.messaging.forEach(function(event) {
+        if (event.message) {
+          receivedMessage(event);
+        } else {
+          console.log("Webhook received unknown event: ", event);
+        }
+      });
+    });
+
+    // Assume all went well.
+    //
+    // You must send back a 200, within 20 seconds, to let us know
+    // you've successfully received the callback. Otherwise, the request
+    // will time out and we will keep trying to resend.
+    res.sendStatus(200);
+  }
+});
+  
+function receivedMessage(event) {
+  // Putting a stub for now, we'll expand it in the following steps
+  console.log("Message data: ", event.message);
+}
+
+function receivedMessage(event) {
+  var senderID = event.sender.id;
+  var recipientID = event.recipient.id;
+  var timeOfMessage = event.timestamp;
+  var message = event.message;
+
+  console.log("Received message for user %d and page %d at %d with message:", 
+    senderID, recipientID, timeOfMessage);
+  console.log(JSON.stringify(message));
+
+  var messageId = message.mid;
+
+  var messageText = message.text;
+  var messageAttachments = message.attachments;
+
+  if (messageText) {
+
+    // If we receive a text message, check to see if it matches a keyword
+    // and send back the example. Otherwise, just echo the text we received.
+    switch (messageText) {
+      case 'generic':
+        sendGenericMessage(senderID);
+        break;
+
+      default:
+        sendTextMessage(senderID, messageText);
+    }
+  } else if (messageAttachments) {
+    sendTextMessage(senderID, "Message with attachment received");
+  }
+}
+
 aidahCare.listen(aidahCare.get('port'), function(){
 	console.log('running on port', aidahCare.get('port'))
 })
